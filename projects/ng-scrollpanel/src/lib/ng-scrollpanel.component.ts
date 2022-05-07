3b5954314e46f377ee1dc1a5760d169bffef2ddb
Mark Goodwin <m0good04@gmail.com>
2022-05-07 15:59:56 -0400

    Scrollbars are now displayed on and off as needed.

--- projects/ng-scrollpanel/src/lib/ng-scrollpanel.component.ts
+++ projects/ng-scrollpanel/src/lib/ng-scrollpanel.component.ts
@@ -1,27 +1,45 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, DoCheck, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'ng-scrollpanel',
   templateUrl: './ng-scrollpanel.component.html',
   styleUrls: ['./ng-scrollpanel.component.css']
 })
-export class NgScrollpanelComponent implements OnInit, AfterViewInit {
+export class NgScrollpanelComponent implements OnInit, AfterViewInit, DoCheck {
   @ViewChild('content') content?: ElementRef;
+  @ViewChild('vscroll') vscroll?: ElementRef;
+  @ViewChild('hscroll') hscroll?: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    console.log('content:', this.content)
     this.content!.nativeElement.onscroll = this.onScroll
   }
 
-  onScroll = (event: any): void => {
+  ngDoCheck(): void {
+    if (!this.content) return;
+
+    let displayVerticalScrollbar = this.content?.nativeElement.scrollHeight > this.content?.nativeElement.clientHeight;
+    let displayHorizontalScrollbar = this.content?.nativeElement.scrollWidth > this.content?.nativeElement.clientWidth;
+
+    this.turnScrollbarOnAndOff(displayVerticalScrollbar, this.vscroll);
+    this.turnScrollbarOnAndOff(displayHorizontalScrollbar, this.hscroll);
+  }
+
+  private onScroll = (event: any): void => {
     const number = event.srcElement.scrollTop;
     console.log(event)
     console.log('I am scrolling ' + number);
+  }
+
+  private turnScrollbarOnAndOff(display: boolean, scrollbar?: ElementRef) {
+    if (display) {
+      scrollbar?.nativeElement.setAttribute('style', 'display: block');
+    } else if (this.content) {
+      scrollbar?.nativeElement.setAttribute('style', 'display: none');
+    }
   }
 
 }
