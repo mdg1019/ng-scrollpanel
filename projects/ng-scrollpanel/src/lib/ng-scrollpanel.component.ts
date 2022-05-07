9971faddde2183c5d002056b6122f84d68af5e6d
Mark Goodwin <m0good04@gmail.com>
2022-05-07 15:31:12 -0400

    Went dark mode and hid the native scrollbars.

--- projects/ng-scrollpanel/src/lib/ng-scrollpanel.component.ts
+++ projects/ng-scrollpanel/src/lib/ng-scrollpanel.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'ng-scrollpanel',
   templateUrl: './ng-scrollpanel.component.html',
   styleUrls: ['./ng-scrollpanel.component.css']
 })
-export class NgScrollpanelComponent implements OnInit {
+export class NgScrollpanelComponent implements OnInit, AfterViewInit {
+  @ViewChild('content') content?: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    console.log('content:', this.content)
+    this.content!.nativeElement.onscroll = this.onScroll
+  }
+
+  onScroll = (event: any): void => {
+    const number = event.srcElement.scrollTop;
+    console.log(event)
+    console.log('I am scrolling ' + number);
   }
 
 }
